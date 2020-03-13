0862ee804f1e54f5cc2f1791505679c123cc021d
paycal <30388769+paycal@users.noreply.github.com>
2020-03-13 10:30:04 +0900

    first commit

---
+++ src/main/java/com/study/ecommerce/springbootecommerce/SpringBootEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.study.ecommerce.springbootecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcommerceApplication.class, args);
+	}
+
+}
